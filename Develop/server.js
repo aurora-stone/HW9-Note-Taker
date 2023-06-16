31f170a42dd343512fdcd3536d03ccee63615321
Aurora Stone <aurorastone@MacBook-Pro.local>
2023-06-16 17:25:31 -0600

    server fix

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const uniqid = require('uniqid')
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static("public"))
